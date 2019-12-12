80614d50f721cfec18443e1900b9a736d2c509c3
Salih Şengönül <salih.sengonul@hotmail.com>
2019-12-12 20:10:50 +0300

    Dal oluşturuldu

---
+++ NBUY_MOD.DAL/Database/ProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBUY_MOD.Entities;
+
+namespace NBUY_MOD.DAL.Database
+{
+    public class ProjectContext:DbContext
+    {
+        public ProjectContext()
+        {
+            
+        }
+
+        public DbSet<News> News { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ NBUY_MOD.Entities/News.cs
@@ -0,0 +1,15 @@
+﻿namespace NBUY_MOD.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string NewsName { get; set; }
+        public string NewsDescription { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsSubTitle { get; set; }
+        public string NewsContent { get; set; }
+        public string NewsImagePath { get; set; }
+        public int CategoryId { get; set; }
+        public  Category Category { get; set; }
+    }
+}
